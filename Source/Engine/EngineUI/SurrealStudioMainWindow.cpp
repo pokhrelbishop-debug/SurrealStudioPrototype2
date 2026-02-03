16e4a49cf3ed824631acaa5bc1f01c503946a32f
bishoppokhrel <pokhrelbishop@gmail.com>
2026-02-03 19:00:12 +0545

    Added EngineUI folder

---
+++ Source/Engine/EngineUI/SurrealStudioMainWindow.cpp
@@ -0,0 +1,14 @@
+#include "SurrealStudioMainWindow.h"
+
+#define GLFW_INCLUDE_NONE
+
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+
+namespace SurrealStudio {
+
+	namespace EngineUI {
+
+		
+	}
+}
---
+++ Source/Engine/EngineUI/SurrealStudioMainWindow.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define GLFW_INCLUDE_NONE
+
+#include "GLFW/glfw3.h"
+
+namespace SurrealStudio {
+
+	namespace EngineUI {
+
+		class SurrealStudioMainWindow
+		{
+		public:
+
+			bool InitWindow();
+			bool CreateWindow(int width, int height, const char* title);
+			bool MainLoop();
+			bool DestroyWindow();
+
+		private:
+
+			GLFWwindow* m_SurrealStudioWindow = nullptr;
+		};
+	}
+}
